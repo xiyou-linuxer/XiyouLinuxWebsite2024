92491aed5882676b7e70ba5c2ee211f4c4abe86c
L33Z22L11 <L33Z22L11@gmail.com>
2024-06-02 21:33:18 +0800

    update: weekly share， member data

--- docs/.vitepress/components/memberData.js
+++ docs/.vitepress/components/memberData.js
@@ -18,7 +18,7 @@ export default {
     { name: '张奕宸', title: '', qq: '2976002717', github: 'erthern', linkText: 'CSDN', link: 'https://blog.csdn.net/thunderblue' },
   ],
   '2022级': [
-    { name: '袁小天', title: '22级负责人', qq: '1179188517', github: 'tiny-sky', linkText: 'CSDN', link: 'https://blog.csdn.net/m0_74114450' },
+    { name: '袁小天', title: '22级负责人', qq: '1179188517', github: 'tiny-sky', linkText: '独立博客', link: 'https://tiny-sky.github.io' },
     { name: '张敏睿', title: '22级负责人', qq: '2743377630', github: 'JINGzmr', linkText: 'CSDN', link: 'https://blog.csdn.net/weixin_74056357' },
     { name: '祝贺琴', title: '22级负责人', qq: '2528487925', github: 'zhuheqqq', linkText: 'CSDN', link: 'https://blog.csdn.net/weixin_74111699' },
     { name: '刘子璐', title: '', qq: '2399052066', github: 'L33Z22L11', linkText: '独立博客', link: 'https://blog.zhilu.cyou/' },
